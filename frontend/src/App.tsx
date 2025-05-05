47059dd0195a3731728ec734097ec80cceaa8a05
iago <iago.cesar@live.com>
2025-05-05 20:24:14 -0300

    fix path alias issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-amber-900 text-foreground">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex flex-col bg-background">
+        <Button onClick={() => setCount((count) => count + 1)}>
           up
-        </button>
-        <button className='bg-amber-600' onClick={() => setCount((count) => count - 1)}>
+        </Button>
+        <Button onClick={() => setCount((count) => count - 1)}>
           down
-        </button>
+        </Button>
         <p>
           {count}
         </p>
